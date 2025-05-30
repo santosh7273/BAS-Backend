87b6fd5714e526cd1e29c0f6b90d1c35a1c348d9
santosh7273 <ktsantosh5@gmail.com>
2025-05-30 06:29:27 +0530

    omg

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
