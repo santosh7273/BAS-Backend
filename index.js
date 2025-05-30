01b265e8b924d189d028cca0b439820aff31a25f
santosh7273 <ktsantosh5@gmail.com>
2025-05-30 06:21:27 +0530

    dk2

--- index.js
+++ index.js
@@ -154,7 +154,7 @@ app.get("/mylistings/:id", authenticate, async (req, res) => {
     const product = user.Products.id(req.params.id);
     if (!product) return res.status(404).send("Product not found");
 
-    console.log(product.collegename);
+    console.log("product.collegename");
     return res.json(product);
   } catch (err) {
     console.error(err);
