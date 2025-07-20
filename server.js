4ba164a26503380b958a42642cfe3813c2ebbe6e
santosh7273 <ktsantosh5@gmail.com>
2025-07-20 17:22:25 +0530

    sample

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Successfully connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
-app.use("/api", routes);
+app.use("/", routes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
