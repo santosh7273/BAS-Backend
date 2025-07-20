97853e5fb08f47d9b7358f9bc8389f4b3abf5828
santosh7273 <ktsantosh5@gmail.com>
2025-07-20 17:00:12 +0530

    sample

---
+++ server.js
@@ -0,0 +1,15 @@
+
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("./routes/index");
+const app = express();
+app.use(cors());
+app.use(express.json());
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Successfully connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
+app.use("/api", routes);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
